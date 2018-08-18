29343e2872434b633c481fc3b3d60ff29730644e
Bryan Appleby <b.appleby@f5.com>
2018-08-18 22:31:19 +0300

    JS / Python polyglot

---
+++ pyjs_polyglot.js.py
@@ -0,0 +1,11 @@
+1 // 1 ; """ polyglot: Don't remove this line
+
+//Javascript goes here
+console.log('javascript');
+
+/* polyglot: Don't remove this line """
+
+# Python goes here
+print("python")
+
+# polyglot: Don't remove this line */
